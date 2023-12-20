09c05c04ae4e781e7785aa185a6e83e6145c0e1d
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-12-20 12:36:56 +0600

    added user data to the API

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const { request } = require('express');
-const express = require('express')
+const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // CALLING CORS
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // BASE API CALL
 app.get('/', (req, res) => {
